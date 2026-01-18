4b8fc9acde98542c7ccb11b070e3dacf27fd7e16
tinkerborg <tinkerborg@users.noreply.github.com>
2026-01-18 11:24:23 -0500

    test

--- flows/trigger_deployment.py
+++ flows/trigger_deployment.py
@@ -3,6 +3,8 @@ from prefect.deployments import run_deployment
 from uuid import UUID
 from prefect.client.schemas.filters import FlowRunFilter, DeploymentFilter, FlowRunFilterTags
 
+tag = "initial-deploy"
+
 @flow(log_prints=True)
 def trigger_deployment(deployment_id: str):
     """
@@ -11,30 +13,31 @@ def trigger_deployment(deployment_id: str):
     print(f"Received event for new deployment: {deployment_id}")
 
     with get_client(sync_client=True) as client:
-        flow_runs = client.read_flow_runs(
-            flow_run_filter=FlowRunFilter(
-                deployment_id={"any_": [deployment_id]},
-                tags=FlowRunFilterTags(
-                    all_=["initial-deployment-automation"]
-                )
-            ),
-            limit=100
-        )
+        deployment = client.read_deployment(deployment_id)
+        
+        if tag in deployment.tags:
+            flow_runs = client.read_flow_runs(
+                flow_run_filter=FlowRunFilter(
+                    deployment_id={"any_": [deployment_id]},
+                    tags=FlowRunFilterTags(
+                        all_=[tag]
+                    )
+                ),
+                limit=100
+            )
 
-        if len(flow_runs) == 0:
-            deployment = client.read_deployment(deployment_id)
-        
-            # Only run if it has the tag
-            if "initial-deployment" in deployment.tags:
-                flow_run = client.create_flow_run_from_deployment(
-                    deployment_id=deployment_id,
-                    labels={"triggered-by": "initial-deployment-automation"},
-                    tags=["initial-deployment-automation"]
-                )
-        
-                print(f"Successfully triggered run {flow_run.id} for deployment {deployment_id}")
-        else: 
-            print(f"Did not trigger deployment {deployment_id}")
+            if len(flow_runs) == 0:
+            
+                # Only run if it has the tag
+                    flow_run = client.create_flow_run_from_deployment(
+                        deployment_id=deployment_id,
+                        # labels={"triggered-by": "initial-deployment-automation"},
+                        # tags=["initial-deployment-automation"]
+                    )
+            
+                    print(f"Successfully triggered run {flow_run.id} for deployment {deployment_id}")
+            else: 
+                print(f"Did not trigger deployment {deployment_id}")
 
 
 if __name__ == "__main__":
